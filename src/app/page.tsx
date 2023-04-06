b4a6505e56925efb11a98db55b7e3843cfea3e5c
Yonathan Cruz <yonathancruz2019@gmail.com>
2023-04-06 14:34:29 -0600

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,72 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import logo from "public/images/onchain.jpeg";
+import styles from "@/styles/home.module.css";
+const { Header, Content, Footer } = Layout;
 
-const inter = Inter({ subsets: ['latin'] })
+const App: React.FC = () => {
+  const router = useRouter();
 
-export default function Home() {
+  const menuItems = [
+    {
+      key: '/',
+      label: <Image
+        src={logo}
+        onClick={() => router.push("/")}
+        alt='logo'
+        width={63}
+        className='main-logo'
+        height={63}
+      />,
+      showOnRedirectPage: true,
+    },
+    {
+      key: '/create',
+      label: "Create Event",
+      onClick: () => router.push("/create"),
+    },
+    {
+      key: '/history',
+      label: "View Events",
+      onClick: () => router.push("/history"),
+    },
+  ]
+
+  const path = "/";
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Layout className="layout">
+      <Header>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          selectedKeys={[path]}
+          items={false ? menuItems.filter(item => item.showOnRedirectPage) : menuItems}
+
+        ></Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className={styles.container}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content" style={{ background: colorBgContainer }}>
+            Content
+          </div>
         </div>
-      </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+    </Layout>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
