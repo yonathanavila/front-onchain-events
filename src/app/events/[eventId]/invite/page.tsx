1b3c7e0c0eb211344a0259bf24a441d4b47c6745
Yonathan Cruz <yonathancruz2019@gmail.com>
2023-04-13 23:55:26 -0600

    fix invitation

--- src/app/events/[eventId]/invite/page.tsx
+++ src/app/events/[eventId]/invite/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import useSWR from 'swr';
 import { usePathname } from 'next/navigation';
-import { Card, Col, Row, Typography, Input, Skeleton, Button } from "antd";
+import { Card, Col, Row, Typography, Input, Skeleton, Button, notification } from "antd";
 import { UserOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const baseURI = process.env.NEXT_PUBLIC_API || '/api/v1/T2';
 
@@ -10,28 +11,61 @@ export default function AttendEvent() {
 
     const { Title } = Typography;
     const router = usePathname();
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState<any>();
+    const [error, setError] = useState<any>();
+
     const pid = router?.split("/")[2].toString();
-    const { data: apiCall, error, isLoading } = useSWR(`${baseURI}/${pid}`);
+    const { data: apiCall, error: ErrorSWR, isLoading } = useSWR(`${baseURI}/${pid}`);
+
+    const handleEmail = (e: any) => {
+        setEmail(e.target.value);
+    }
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
+        setLoading(true);
 
-        // post to /api/v1/Mail
-        fetch('/api/v1/Mail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userMailChain: 'yonathan@mailchain',
-                link: 'https://mailchain.xyz'
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Success:', data);
-            });
+        try {
+            const newData_ = {
+                "userMailChain": email,
+                "id": pid,
+            }
+            // post to /api/v1/Mail
+            fetch('/api/v1/Mail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newData_),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Success:', data);
+                    openNotification({
+                        message: 'Success',
+                        description: 'Your invitation has been sent.',
+                    });
+                    if (data?.error) {
+                        setError(data?.error);
+                    }
+                });
+        } catch (error) {
+            console.log(error);
+            setError(error);
+        }
+        setLoading(false);
     }
+
+    const openNotification = ({ message, description }: any) => {
+        notification.open({
+            message,
+            description,
+            onClick: () => {
+                console.log('Notification Clicked!');
+            },
+        });
+    };
 
     return (
         <>
@@ -46,11 +80,26 @@ export default function AttendEvent() {
                             <Title level={4}>
                                 {apiCall[0]?.description}
                             </Title>
-                            <Input size="large" placeholder="vitalik" prefix={<UserOutlined />} suffix="@mailchain.com" />
+                            <Input size="large" value={email} name="email" onChange={handleEmail} placeholder="vitalik" prefix={<UserOutlined />} suffix="@mailchain.com" />
                             <br />
                             <br />
                             <br />
-                            <Button type="primary" size="large" onClick={handleSubmit}>Invite</Button>
+                            <Button
+                                style={{ backgroundColor: '#520339', border: 'none', color: '#fff' }}
+                                disabled={loading}
+                                loading={loading}
+                                size="large"
+                                onClick={handleSubmit}
+                            >
+                                Invite
+                            </Button>
+                            {!error && !apiCall && isLoading && (
+                                <span>&nbsp;Note this may take a few moments.</span>
+                            )}
+                            {error && <div>
+                                <div className="error-text">{error}</div>
+                            </div>
+                            }
                             <br />
                             <br />
                         </Card>
